3034b881fe422b9800b67386cb48a24ecf9526c6
Icaruk <adri-rk@hotmail.com>
2020-12-04 22:55:25 +0100

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# 🌧 Lluvia
+
+Esta es mi solución al problema de **Trapping Rain Water** _(atrapando agua de lluvia)_.
+
+---
+
+#### 🚀 ¡Pruébalo desde este [link](www.asd.es)!
+
+---
+
+![](https://i.gyazo.com/3069d630813dbc57f8c79cfcc033943e.gif)
+
---
+++ index.js
@@ -0,0 +1,140 @@
+
+// Constantes
+const longitudX = 8;
+const longitudY = 5;
+
+
+// Variables
+let alturas = {};
+
+
+
+const generaBloques = () => {
+	
+	const main = document.querySelector(".fila");
+	
+	
+	for (let _columna = 0; _columna < longitudX; _columna ++) {
+		
+		let columna = document.createElement("div");
+		columna.className = "columna";
+		
+		
+		for (let _altura = 0; _altura < longitudY; _altura ++) {
+			
+			let bloque = document.createElement("div");
+			bloque.className = "bloque";
+			bloque.onclick = () => pulsaBloque(_columna, _altura);
+			bloque.id = `${_columna}_${_altura}`;
+			
+			columna.appendChild(bloque);
+			
+		};
+		
+		main.appendChild(columna);
+		alturas[_columna] = 0;
+		
+	};
+	
+};
+
+
+
+const pulsaBloque = (x, y) => {
+	
+	alturas[x] = y;
+	
+	pintaBloques();
+	pintaAgua();
+};
+
+
+
+const pintaBloques = () => {
+	
+	for (let _columna = 0; _columna < longitudX; _columna ++) {
+		for (let _altura = 0; _altura < longitudY; _altura ++) {
+			
+			let bloque = document.getElementById( `${_columna}_${_altura}`);
+			
+			if (alturas[_columna] >= _altura) {
+				bloque.className = " bloque bloque-pintado";
+			} else {
+				bloque.className = "bloque bloque-oculto";
+			}
+			
+		};
+	};
+	
+};
+
+
+
+const pintaAgua = () => {
+	
+	// Recorro las filas de arriba a abajo
+	for (let _altura = longitudY; _altura > 0; _altura --) {	
+		
+		console.log( `------ Altura ${_altura} ------` );
+		
+		let idxPrimerBloque = null;
+		let idxSegundoBloque = null;
+		
+		
+		// Recorro los índices de la fila de 0 a X
+		for (let _columna = 0; _columna < longitudX; _columna ++) {
+			
+			let alturaActual = alturas[_columna];
+			
+			
+			// Compruebo si es un bloque
+			if (_altura <= alturaActual) {
+				
+				if (idxPrimerBloque === null) { // Si no tengo primer bloque, lo pongo
+					idxPrimerBloque = _columna;
+				} else { // Si ya tengo el primero, pongo el segundo
+					idxSegundoBloque = _columna;
+				};
+				
+				
+				if ((idxSegundoBloque - idxPrimerBloque) > 0) {
+					pintaFilaAgua(_altura, idxPrimerBloque + 1, idxSegundoBloque - 1)
+				};
+				
+			};
+			
+		};
+		
+	};
+	
+};
+
+
+
+const pintaFilaAgua = (altura, desde, hasta) => {
+	
+	for (let _columna = desde; _columna <= hasta; _columna ++) {
+		
+		let bloque = document.getElementById( `${_columna}_${altura}`);
+		
+		if (alturas[_columna] < altura) bloque.className = " bloque bloque-agua";
+		
+	};
+	
+};
+
+
+
+const pulsaVaciarTodo = () => {
+	
+	for (_key in alturas) {
+		alturas[_key] = 0;
+	};
+	
+	pintaBloques();
+	
+};
+
+
+generaBloques();
+pintaBloques();
